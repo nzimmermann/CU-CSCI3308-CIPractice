2138dc66a5c739fc7cc0ceeccc61fd2200ac50d8
user <nickmanpr@gmail.com>
2015-04-15 19:31:05 -0600

    Made area test! fuckin a

--- geometry_test.c
+++ geometry_test.c
@@ -15,6 +15,23 @@
 
 #include "geometry.h"
 
+START_TEST(test_2d_area)
+{
+	coord_2d_t a;
+	coord_2d_t b;
+	coord_2d_t c;
+	
+	a.x = 23;
+	a.y = 16;
+	b.x = 23;
+	b.y = 30;
+	c.x = 60;
+	c.y = 30;
+	ck_assert(coord_2d_area_triangle(&a, &b, &c) == 259);
+	
+	}
+END_TEST
+
 /* coord_2d_eq Test */
 START_TEST(test_2d_eq)
 {
@@ -161,11 +178,15 @@ Suite* coord_2d_suite(void)
 
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
+    
+    TCase* tc_2d_area = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area, test_2d_area);
 
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
